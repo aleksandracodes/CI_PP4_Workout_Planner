7b41c3869221125f7f593d21f67d9fb08c6e0cc0
Aleksandra <a.haniok@gmail.com>
2022-03-29 16:54:25 +0100

    Create the draft index page

--- workout_planner/settings.py
+++ workout_planner/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ci-pp4-workout-planner.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['ci-pp4-workout-planner.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
