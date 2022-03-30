a4a4c074f23b8f721024dff743c8896fbc551954
Aleksandra <a.haniok@gmail.com>
2022-03-30 17:33:57 +0100

    Prepare draft exercises models and check functionality in admin area

--- exercises/admin.py
+++ exercises/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Exercise, BodyPart, Type, Equipment, Level
 
-# Register your models here.
+
+admin.site.register(Exercise)
+admin.site.register(BodyPart)
+admin.site.register(Type)
+admin.site.register(Equipment)
+admin.site.register(Level)
--- exercises/models.py
+++ exercises/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class Exercise(models.Model):
+    name = models.CharField(max_length=150, unique=True)
+    description = models.TextField()
+    body_part = models.CharField(max_length=50)
+    type = models.CharField(max_length=50)
+    equipment = models.CharField(max_length=50)
+    level = models.CharField(max_length=50)
+    image = CloudinaryField('image', default='placeholder')
+
+    def __str__(self):
+        return self.name
+
+
+class BodyPart(models.Model):
+    part = models.CharField(max_length=50, unique=True)
+    
+    def __str__(self):
+        return self.part
+
+
+class Type(models.Model):
+    type = models.CharField(max_length=50, unique=True)
+    
+    def __str__(self):
+        return self.type
+
+
+class Equipment(models.Model):
+    equipment = models.CharField(max_length=50, unique=True)
+    
+    def __str__(self):
+        return self.equipment
+
+
+class Level(models.Model):
+    level = models.CharField(max_length=50, unique=True)
+    
+    def __str__(self):
+        return self.level
