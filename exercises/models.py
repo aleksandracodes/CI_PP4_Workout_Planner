7243638c66803871901f36c08152b382057cfefc
Aleksandra <a.haniok@gmail.com>
2022-03-31 08:46:39 +0100

    Amend the exercise model with correct relationships

--- exercises/models.py
+++ exercises/models.py
@@ -2,18 +2,6 @@ from django.db import models
 from cloudinary.models import CloudinaryField
 
 
-class Exercise(models.Model):
-    name = models.CharField(max_length=150, unique=True)
-    description = models.TextField()
-    body_part = models.CharField(max_length=50)
-    type = models.CharField(max_length=50)
-    equipment = models.CharField(max_length=50)
-    level = models.CharField(max_length=50)
-    image = CloudinaryField('image', default='placeholder')
-
-    def __str__(self):
-        return self.name
-
 
 class BodyPart(models.Model):
     part = models.CharField(max_length=50, unique=True)
@@ -41,3 +29,16 @@ class Level(models.Model):
     
     def __str__(self):
         return self.level
+
+
+class Exercise(models.Model):
+    name = models.CharField(max_length=150, unique=True)
+    description = models.TextField()
+    body_part = models.ForeignKey(BodyPart, on_delete=models.CASCADE)
+    type = models.ForeignKey(Type, on_delete=models.CASCADE)
+    equipment = models.ManyToManyField(Equipment)
+    level = models.ForeignKey(Level, on_delete=models.CASCADE)
+    image = CloudinaryField('image', default='placeholder')
+
+    def __str__(self):
+        return self.name
