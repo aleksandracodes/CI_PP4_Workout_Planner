d0dbda9b998a6c60be53c7dfc85ce10a75e2a582
Aleksandra <a.haniok@gmail.com>
2022-04-01 10:27:08 +0100

    Modify admin area to display ordered names & search field

--- exercises/admin.py
+++ exercises/admin.py
@@ -2,8 +2,13 @@ from django.contrib import admin
 from .models import Exercise, BodyPart, Type, Equipment, Level
 
 
-admin.site.register(Exercise)
 admin.site.register(BodyPart)
 admin.site.register(Type)
 admin.site.register(Equipment)
 admin.site.register(Level)
+
+@admin.register(Exercise)
+class ExerciseAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body_part', 'level')
+    ordering = ('name',)
+    search_fields = ('name',)
