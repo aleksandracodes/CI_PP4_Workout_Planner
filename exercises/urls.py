fd2a9703415b46c5bbfe0421751e8436271bc96a
Aleksandra <a.haniok@gmail.com>
2022-04-01 13:45:03 +0100

    Create a single exercise page view

--- exercises/urls.py
+++ exercises/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from exercises.views import ExercisesView
+from . import views
 
 urlpatterns = [
-    path('', ExercisesView.as_view()),
+    path('', views.ExercisesView.as_view()),
+    path('show_exercise/<exercise_id>', views.SingleExercise.as_view(), name='show_exercise')
 ]
--- exercises/views.py
+++ exercises/views.py
@@ -1,7 +1,15 @@
-from django.views.generic import ListView
+from django.shortcuts import render
+from django.views.generic import ListView, View
 from exercises.models import Exercise
 
 
 class ExercisesView(ListView):
     model = Exercise
-    template_name = "exercises/exercises.html"
+    template_name = "exercises/exercises-list.html"
+
+
+class SingleExercise(View):
+    template_name = 'exercises/exercise.html'
+    def get(self, request, exercise_id, **kwargs):
+        single_exercise = Exercise.objects.get(pk=exercise_id)
+        return render(request, 'exercises/exercise.html', {'single_exercise': single_exercise})
