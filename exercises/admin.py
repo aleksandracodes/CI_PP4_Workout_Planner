1afeb70c214f81c004231c50f78f2e20a26236a8
Aleksandra <a.haniok@gmail.com>
2022-04-01 16:47:04 +0100

    Install summernote library to edit exercise  description in admin

--- exercises/admin.py
+++ exercises/admin.py
@@ -1,14 +1,16 @@
 from django.contrib import admin
 from .models import Exercise, BodyPart, Type, Equipment, Level
-
+from django_summernote.admin import SummernoteModelAdmin
 
 admin.site.register(BodyPart)
 admin.site.register(Type)
 admin.site.register(Equipment)
 admin.site.register(Level)
 
+
 @admin.register(Exercise)
-class ExerciseAdmin(admin.ModelAdmin):
+class ExerciseAdmin(SummernoteModelAdmin):
     list_display = ('name', 'body_part', 'level')
     ordering = ('name',)
     search_fields = ('name',)
+    summernote_fields = ('description')
--- exercises/urls.py
+++ exercises/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.ExercisesView.as_view()),
+    path('', views.ExercisesView.as_view(), name='exercises'),
     path('show_exercise/<exercise_id>', views.SingleExercise.as_view(), name='show_exercise')
 ]
