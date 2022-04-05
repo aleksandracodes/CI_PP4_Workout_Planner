bebf02c1d99834c716e3e82705dfdbbb833eefd3
Aleksandra <a.haniok@gmail.com>
2022-04-05 19:00:25 +0100

    Render planner main page

--- plannerapp/views.py
+++ plannerapp/views.py
@@ -1,3 +1,12 @@
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3rd party:
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+class WorkoutPlannerView(ListView):
+    def get(self, request):
+            return render(request, 'plannerapp/planner.html')
