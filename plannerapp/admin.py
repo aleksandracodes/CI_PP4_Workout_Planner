ef1257b895002f1ed8fb9aede736a3a5e1d68545
Aleksandra <a.haniok@gmail.com>
2022-04-05 14:34:43 +0100

    Register models in admin.py with basic display

--- plannerapp/admin.py
+++ plannerapp/admin.py
@@ -4,9 +4,22 @@
 from django.contrib import admin
 
 # Internal:
-from .models import WorkoutPlan, WorkoutTime, Workout
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+from .models import WorkoutTime, WorkoutPlan, Workout
+
 
 admin.site.register(WorkoutPlan)
-admin.site.register(WorkoutTime)
-admin.site.register(Workout)
+
+
+@admin.register(WorkoutTime)
+class WorkoutTimeAdmin(admin.ModelAdmin):
+    list_display = ('workout_time_name',)
+
+
+@admin.register(Workout)
+class WorkoutAdmin(admin.ModelAdmin):
+    list_display = (
+        'workout_name', 
+        'workout_time', 
+        'workout_plan', 
+        'day'
+    )
