c8fac7b15fe5d14e57c6e809191053ea8121f41a
Aleksandra <a.haniok@gmail.com>
2022-04-05 08:52:02 +0100

    Add basic planner model

--- planner/admin.py
+++ planner/admin.py
@@ -1,3 +1,12 @@
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3rd party:
 from django.contrib import admin
 
-# Register your models here.
+# Internal:
+from .models import WorkoutPlan, WorkoutType, Workout
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+admin.site.register(WorkoutPlan)
+admin.site.register(WorkoutType)
+admin.site.register(Workout)
--- planner/models.py
+++ planner/models.py
@@ -1,3 +1,46 @@
+# Imports
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# 3rd party:
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class WorkoutTime(models.Model):
+    workout_time = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.workout_time
+
+class WorkoutPlan(models.Model):
+    day = models.DateTimeField()
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='workout_plan'
+    )
+
+    def __str__(self):
+        return self.pk
+    
+    
+class Workout(models.Model):
+    workout_name = models.CharField(
+        max_length=100,
+        null=False,
+        default=''
+    )
+    workout_time = models.ForeignKey(
+        WorkoutTime,
+        on_delete=models.CASCADE,
+        related_name='workout_time'
+    )
+    workout_plan = models.ForeignKey(
+        WorkoutPlan,
+        on_delete=models.CASCADE,
+        related_name='workout'
+    )
+    day = models.DateTimeField()
+    
+
+    def __str__(self):
+        return self.workout_name
