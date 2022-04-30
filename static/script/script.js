a415d3785d9b9cda1fecbdec8bd54827aa13f668
Aleksandra <a.haniok@gmail.com>
2022-04-30 10:28:25 +0100

    Remove duplicated filters menu and adjust dropdown on small screens with JS

--- static/script/script.js
+++ static/script/script.js
@@ -1,5 +1,6 @@
-// Automatically dismiss messages
+/*jshint esversion: 6 */
 
+// Automatically dismiss messages
 setTimeout(function () {
     let messages = document.getElementById('msg');
     let alert = new bootstrap.Alert(messages);
@@ -9,9 +10,16 @@ setTimeout(function () {
 
 // Dropdown exercises filters
 $(document).ready(function(){
-    $(".show-filters-small").hide();
     $(".expand").click(function(){
         $(this).parent().next().slideToggle(600);
     
     });
 });
+
+const filters = document.querySelector('.filter-exercises');
+const adjustDisplay = () => {
+    const width = window.innerWidth;
+    filters.style.display = width > 502 ? 'block' : 'none';
+};
+window.onload = () => adjustDisplay();
+window.onresize = () => adjustDisplay();
